d43fe0b7a1b905a0d49edd8eb6a874572a14f29d
ahmadesam <ahmadesamsuleiman@gmail.com>
2025-08-10 06:00:21 +0300

    version 0.2

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -8,7 +8,6 @@ port="$2"
 shift 2
 cmd="$@"
 
-# Check if the service is available by checking its port
 until nc -z "$host" "$port"; do
   >&2 echo "$host:$port is unavailable - sleeping"
   sleep 1
